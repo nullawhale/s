395007eb52da0d9e44891eb15402efbd9b658690
nullawhale <kura98.21@gmail.com>
2021-04-12 15:58:14 +0300

    moduls

---
+++ apple.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/veandco/go-sdl2/sdl"
+
+type Apple struct {
+	x, y int32
+	size int32
+}
+
+func (a *Apple) new() {
+
+}
+
+func (a *Apple) draw(renderer *sdl.Renderer) (err error) {
+	var outlineRect = sdl.Rect{X: a.x, Y: a.y, W: a.size, H: a.size}
+	if err = renderer.SetDrawColor(255, 0, 0, 255); err != nil {
+		return err
+	}
+	if err = renderer.FillRect(&outlineRect); err != nil {
+		return err
+	}
+
+	return
+}
